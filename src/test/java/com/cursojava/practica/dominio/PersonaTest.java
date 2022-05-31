77d880f1bb2bafaca0c0dc14ddd591fec1df82e4
prubioamzubiri <prubioam@zubirimanteo.com>
2022-05-31 10:54:25 +0200

    Test creado

---
+++ src/test/java/com/cursojava/practica/dominio/PersonaTest.java
@@ -0,0 +1,14 @@
+package com.cursojava.practica.dominio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonaTest {
+    @Test
+    void testHBirthday() {
+
+        assertEquals(true, false);
+
+    }
+}
