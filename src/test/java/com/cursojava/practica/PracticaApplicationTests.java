ba845a6bdbbbf9215865c754baef2a6f9bd2f116
prubioamzubiri <92910817+prubioamzubiri@users.noreply.github.com>
2022-06-28 11:10:39 +0200

    Configurar-ci (#2)
    * Añadida depencia de junit al pom
    
    * añadido test de hBirthday
    
    * Generado fichero GHAction para prueba
    
    * cambiado CI.yaml para ejecutar pwd
    
    * CI.yaml ejecuta ls -l
    
    * añadido checkout a CI.yaml
    
    * añadido mvn test a CI.yaml
    
    * actualizada la version de setup-jdk a v1
    
    * añadido package -DSkipTests
    
    * actualizado Persona.java para que falle hBirthday
    
    * cambido trigger de CI a pullRequest

--- src/test/java/com/cursojava/practica/PracticaApplicationTests.java
+++ src/test/java/com/cursojava/practica/PracticaApplicationTests.java
@@ -1,13 +1,27 @@
 package com.cursojava.practica;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cursojava.practica.dominio.Persona;
 
 @SpringBootTest
 class PracticaApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void testHBirthday()
+	{
+		Persona p = new Persona("0001", "Pepe", 7);
+
+		int expected = 8;
+
+		p.hBirthday();
+
+		assertEquals(expected, p.getAge());
+
 	}
 
 }
